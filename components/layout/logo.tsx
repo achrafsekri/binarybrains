0e1596ad0244e97984f54a48596c723e22f4a0df
achrafsekri <achrafsekri2001@gmail.com>
2024-09-21 12:21:00 +0100

    Update dashboard sidebar layout and add logo

---
+++ components/layout/logo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <a href="/" className="">
+      <span className="text-xl transition duration-300 hover:text-gray-600 font-extrabold text-gray-800">
+        Allo
+        <span className="text-gradient_indigo-purple">Facture</span>
+      </span>
+    </a>
+  );
+};
+
+export default Logo;
