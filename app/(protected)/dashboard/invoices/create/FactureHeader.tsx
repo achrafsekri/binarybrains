fed5ae4dbcfe915db1a4432fb17ce8e4a45ea896
zouno23 <medazizbenhmidene@gmail.com>
2024-10-02 13:56:06 +0100

    added the changeability feature of the invoice header

--- app/(protected)/dashboard/invoices/create/FactureHeader.tsx
+++ app/(protected)/dashboard/invoices/create/FactureHeader.tsx
@@ -1,23 +1,56 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { format } from "date-fns";
 
+import { Input } from "@/components/ui/input";
+import { DatePicker } from "@/components/shared/DatePicker";
+
+import { invoiceFormContext } from "./CreateInvoiceForm";
+
 const FactureHeader = () => {
-  const today = new Date();
-  // 12 October, 2025
-  const formattedDate = format(today, "dd MMMM, yyyy");
+  const [IdFocus, setIdFocus] = useState(false);
+  const form = useContext(invoiceFormContext);
+  const invoiceDetails = form?.getValues("InvoiceDetails");
+  form?.watch("InvoiceDetails");
   return (
     <div>
-      <h1 className="mb-2 text-4xl lg:text-5xl font-semibold text-gray-800">Facture</h1>
+      <h1 className="mb-2 text-4xl font-semibold text-gray-800 lg:text-5xl">
+        Facture
+      </h1>
       <div className="flex flex-wrap items-center gap-2 text-xs lg:gap-4">
-        <div className="rounded-full border-[1px] border-dashed border-gray-800 px-2 py-1">
-          Facture° 0001
-        </div>
-        <div className="rounded-full border-[1px] border-dashed border-gray-800 px-2 py-1">
-          {formattedDate}
-        </div>
-        <div className="rounded-full border-[1px] border-dashed border-gray-800 px-2 py-1">
-          Échéance : 30 / 11 / 2035
-        </div>
+        {!IdFocus ? (
+          <div
+            onDoubleClick={() => setIdFocus(true)}
+            className="rounded-full border border-dashed border-gray-800 px-2 py-1"
+          >
+            Facture° {invoiceDetails?.id}
+          </div>
+        ) : (
+          <Input
+            autoFocus
+            onBlur={() => setIdFocus(false)}
+            className="size-fit rounded-full border border-dashed border-gray-800 px-2 py-1 text-[10px]"
+            value={invoiceDetails?.id}
+            onKeyDown={(e) => e.key === "Enter" && setIdFocus(false)}
+            onChange={(e) =>
+              form?.setValue("InvoiceDetails.id", e.target.value)
+            }
+          />
+        )}
+        <DatePicker
+          className="size-fit rounded-full border border-dashed border-gray-800 px-2 py-1 text-xs"
+          date={invoiceDetails?.startingDate as Date}
+          setDate={(date) =>
+            form?.setValue("InvoiceDetails.startingDate", date as Date)
+          }
+        />
+        <DatePicker
+          className="size-fit rounded-full border border-dashed border-gray-800 px-2 py-1 text-xs"
+          date={invoiceDetails?.dueDate as Date}
+          setDate={(date) =>
+            form?.setValue("InvoiceDetails.dueDate", date as Date)
+          }
+          prefix="Échéance"
+        />
       </div>
       <hr className="my-4" />
     </div>
--- components/shared/DatePicker.tsx
+++ components/shared/DatePicker.tsx
@@ -1,20 +1,31 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { format } from "date-fns"
-import { Calendar as CalendarIcon } from "lucide-react"
+import * as React from "react";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
+} from "@/components/ui/popover";
 
-export function DatePicker({date , setDate , className , withIcon}:{withIcon?:boolean ,className?: string , date: Date | null , setDate: (date: Date | null | undefined) => void}) {
-
+export function DatePicker({
+  date,
+  setDate,
+  className,
+  withIcon,
+  prefix,
+}: {
+  withIcon?: boolean;
+  className?: string;
+  date: Date | null;
+  setDate: (date: Date | null | undefined) => void;
+  prefix?: string;
+}) {
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -22,11 +33,20 @@ export function DatePicker({date , setDate , className , withIcon}:{withIcon?:bo
           variant={"outline"}
           className={cn(
             "w-full justify-start text-left font-normal",
-            !date && "text-muted-foreground", className
+            !date && "text-muted-foreground",
+            className,
           )}
         >
           {withIcon && <CalendarIcon className="mr-2 size-4" />}
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
+          {date ? (
+            prefix ? (
+              `${prefix} ${format(date, "PPP")}`
+            ) : (
+              format(date, "PPP")
+            )
+          ) : (
+            <span>Pick a date</span>
+          )}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
@@ -38,5 +58,5 @@ export function DatePicker({date , setDate , className , withIcon}:{withIcon?:bo
         />
       </PopoverContent>
     </Popover>
-  )
+  );
 }
