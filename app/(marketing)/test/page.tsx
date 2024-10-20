15200f97a0995382f764d692842829340202223f
Achraf Sekri <achrafsekri2001@gmail.com>
2024-10-20 17:23:06 +0100

    feat: Add MinimalTiptapEditor component to test page

---
+++ app/(marketing)/test/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { MinimalTiptapEditor } from '@/components/shared/minimal-tiptap'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <MinimalTiptapEditor />
+    </div>
+  )
+}
+
+export default page
